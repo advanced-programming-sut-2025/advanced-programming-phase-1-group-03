7df13e671d77f7e29f0c819f4cd26326d91347a6
Parsa <parsa.sabzei@gmail.com>
2025-04-13 20:42:06 +0330

    Adding fish and crops features

--- src/main/java/phi/ap/model/enums/CropsFeatures.java
+++ src/main/java/phi/ap/model/enums/CropsFeatures.java
@@ -1,5 +1,36 @@
 package phi.ap.model.enums;
 
-public enum CropsFeatures {
+import phi.ap.model.Eatable;
+import phi.ap.model.enums.Time.Seasons;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public enum CropsFeatures {
+    BlueJazz("Blue Jazz",SeedFeatures.Jazz, (ArrayList<Integer>) Arrays.asList(1, 2, 2, 2), 7,true,, 50, true, 45, Seasons.Spring, false);
+    private String name;
+    private SeedFeatures source;
+    private ArrayList<Integer> stage;
+    private int harvestTime;
+    private boolean onTime;
+    private int regrowthTime;
+    private int baseSellPrice;
+    private Eatable eatable;
+    private int energy;
+    private Seasons season;
+    private boolean canBecomeGiant;
+
+    CropsFeatures(String name, SeedFeatures source, ArrayList<Integer> stage, int harvestTime, boolean onTime, int regrowthTime, int baseSellPrice, Eatable eatable, int energy, Seasons season, boolean canBecomeGiant) {
+        this.name = name;
+        this.source = source;
+        this.harvestTime = harvestTime;
+        this.onTime = onTime;
+        this.regrowthTime = regrowthTime;
+        this.baseSellPrice = baseSellPrice;
+        this.eatable = eatable;
+        this.energy = energy;
+        this.season = season;
+        this.canBecomeGiant = canBecomeGiant;
+        this.stage = stage;
+    }
 }
--- src/main/java/phi/ap/model/enums/FishFeatures.java
+++ src/main/java/phi/ap/model/enums/FishFeatures.java
@@ -1,5 +1,15 @@
 package phi.ap.model.enums;
 
-public enum FishFeatures {
+import phi.ap.model.enums.Time.Seasons;
 
+public enum FishFeatures {
+    Salmon("Salmon", 75, Seasons.Fall);
+    private String name;
+    private int sellPrice;
+    private Seasons season;
+    FishFeatures(String name, int price, Seasons season ) {
+        this.name = name;
+        this.sellPrice = price;
+        this.season = season;
+    }
 }
