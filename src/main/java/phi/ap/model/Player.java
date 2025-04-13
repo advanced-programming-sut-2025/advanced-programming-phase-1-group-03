7288051a264da7653cfd10528a1d8fa49314d01c
MISM06 <iliasardarm85@gmial.com>
2025-04-13 20:36:05 +0330

    phase 0

--- src/main/java/phi/ap/model/Player.java
+++ src/main/java/phi/ap/model/Player.java
@@ -24,4 +24,121 @@ public class Player extends Human {
     private ArrayList<TradeRequest> tradeRequests;
     private ArrayList<Quest> activatedQuests;
 
+    public Player(User user, int energy, Coordinate coordinate, Tool tool, InventoryManager inventoryManager,
+                  ArrayList<Recipe> craftingRecipes, ArrayList<Recipe> cookingRecipes, ArrayList<Animal> animals,
+                  ArrayList<Gift> giftsReceived, ArrayList<MarriageRequest> marriageRequests, ArrayList<TradeRequest>
+                          tradeRequests, ArrayList<Quest> activatedQuests) {
+        this.user = user;
+        Energy = energy;
+        Coordinate = coordinate;
+        this.tool = tool;
+        this.inventoryManager = inventoryManager;
+        this.craftingRecipes = craftingRecipes;
+        this.cookingRecipes = cookingRecipes;
+        this.animals = animals;
+        this.giftsReceived = giftsReceived;
+        this.marriageRequests = marriageRequests;
+        this.tradeRequests = tradeRequests;
+        this.activatedQuests = activatedQuests;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public int getEnergy() {
+        return Energy;
+    }
+
+    public Coordinate getCoordinate() {
+        return Coordinate;
+    }
+
+    public Tool getTool() {
+        return tool;
+    }
+
+    public InventoryManager getInventoryManager() {
+        return inventoryManager;
+    }
+
+    public ArrayList<Recipe> getCraftingRecipes() {
+        return craftingRecipes;
+    }
+
+    public ArrayList<Recipe> getCookingRecipes() {
+        return cookingRecipes;
+    }
+
+    public ArrayList<Animal> getAnimals() {
+        return animals;
+    }
+
+    public ArrayList<Gift> getGiftsReceived() {
+        return giftsReceived;
+    }
+
+    public ArrayList<MarriageRequest> getMarriageRequests() {
+        return marriageRequests;
+    }
+
+    public ArrayList<TradeRequest> getTradeRequests() {
+        return tradeRequests;
+    }
+
+    public ArrayList<Quest> getActivatedQuests() {
+        return activatedQuests;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public void setEnergy(int energy) {
+        Energy = energy;
+    }
+
+    public void setCoordinate(Coordinate coordinate) {
+        Coordinate = coordinate;
+    }
+
+    public void setTool(Tool tool) {
+        this.tool = tool;
+    }
+
+    public void setInventoryManager(InventoryManager inventoryManager) {
+        this.inventoryManager = inventoryManager;
+    }
+
+    public void setCraftingRecipes(ArrayList<Recipe> craftingRecipes) {
+        this.craftingRecipes = craftingRecipes;
+    }
+
+    public void setCookingRecipes(ArrayList<Recipe> cookingRecipes) {
+        this.cookingRecipes = cookingRecipes;
+    }
+
+    public void setAnimals(ArrayList<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void setGiftsReceived(ArrayList<Gift> giftsReceived) {
+        this.giftsReceived = giftsReceived;
+    }
+
+    public void setMarriageRequests(ArrayList<MarriageRequest> marriageRequests) {
+        this.marriageRequests = marriageRequests;
+    }
+
+    public void setTradeRequests(ArrayList<TradeRequest> tradeRequests) {
+        this.tradeRequests = tradeRequests;
+    }
+
+    public void setActivatedQuests(ArrayList<Quest> activatedQuests) {
+        this.activatedQuests = activatedQuests;
+    }
+
+    public void feint() {
+
+    }
 }
