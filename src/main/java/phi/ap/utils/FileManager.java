944bf3de4782a0d629e76cc01d65125faba81c92
ParsaSabzei <parsa.sabzei@gmail.com>
2025-06-16 00:35:08 +0330

    Fixing some bugs in saving users

--- src/main/java/phi/ap/utils/FileManager.java
+++ src/main/java/phi/ap/utils/FileManager.java
@@ -8,16 +8,20 @@ import phi.ap.model.User;
 import phi.ap.model.data.AppData;
 import phi.ap.model.data.LoggedInUser;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Modifier;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileManager {
     private final String path = System.getProperty("user.home") + "/.stardewValley/";
     private final String appDataPath = path + "appData.json";
 
     public void writeAppData(){
+        createIfNotExists();
         AppData data = AppData.build();
         try {
             new java.io.File(appDataPath).getParentFile().mkdirs();
@@ -29,6 +33,7 @@ public class FileManager {
         }
     }
     public AppData readAppData(){
+        createIfNotExists();
         try (FileReader reader = new FileReader(appDataPath)) {
             Gson gson = new Gson();
             return gson.fromJson(reader, AppData.class);
@@ -36,5 +41,17 @@ public class FileManager {
             e.printStackTrace();
         }
         return null;
+    }
+    private void createIfNotExists() {
+        File baseDirectory = new File(path);
+        if(!baseDirectory.exists()){
+            baseDirectory.mkdirs();
+        }
+        File appData = new File(appDataPath);
+        if(!appData.exists()){
+            try (FileWriter writer = new FileWriter(appData)) {
+                writer.write("{}");
+            }catch (IOException ignored){}
+        }
     }
 }
